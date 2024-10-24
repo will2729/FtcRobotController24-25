d2f2224c2c0133c24061e4c1624e1b388ec4c138
CyberEaglesAmethyst1 <147578354+CyberEaglesAmethyst1@users.noreply.github.com>
2024-10-23 19:48:16 -0600

    Update autocode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocode.java
@@ -25,7 +25,18 @@ public class autocode extends LinearOpMode {
          backR.setDirection(DcMotor.Direction.FORWARD);//+
          frontL.setDirection(DcMotor.Direction.REVERSE);//-
          frontR.setDirection(DcMotor.Direction.REVERSE);//+;
-//         straight();
+//
+         waitForStart();
+         straight(300, 0.7, 100);
+         reverse(600,0.7,100);
+         straight(300,0.7,500);
+         strafeLeft(300,0.5,100);
+         strafeRight(600,0.5,100);
+         strafeLeft(300,0.5,500);
+         turn_left(100,0.5,100);
+         turn_right(200,0.5,100);
+         turn_left(100,0.5,500);
+
 //
 
      }
@@ -115,6 +126,47 @@ public class autocode extends LinearOpMode {
 
                 }
 
+                void turn_left(int distance, double power, long sleeptime){
+
+                    backL.setTargetPosition(-distance + backL.getCurrentPosition());
+                    frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
+                    backR.setTargetPosition(distance + backR.getCurrentPosition());
+                    frontR.setTargetPosition(distance + frontR.getCurrentPosition());
+
+                    backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                    backL.setPower(-power);
+                    backR.setPower(power);
+                    frontL.setPower(-power);
+                    frontR.setPower(power);
+
+                    sleep(sleeptime);
+
+
+                    }
+
+                    void turn_right(int distance, double power, long sleeptime){
+
+                        backL.setTargetPosition(distance + backL.getCurrentPosition());
+                        frontL.setTargetPosition(distance + frontL.getCurrentPosition());
+                        backR.setTargetPosition(-distance + backR.getCurrentPosition());
+                        frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
+
+                        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                        backL.setPower(power);
+                        backR.setPower(-power);
+                        frontL.setPower(power);
+                        frontR.setPower(-power);
+
+                        sleep(sleeptime);
+                    }
     }
 
 
