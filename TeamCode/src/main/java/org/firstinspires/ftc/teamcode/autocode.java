35a2aed1791a32422a8b887ada99cadf14fbd7ea
CyberEaglesAmethyst1 <147578354+CyberEaglesAmethyst1@users.noreply.github.com>
2024-10-23 18:51:30 -0600

    autocode
    woo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocode.java
@@ -25,10 +25,9 @@ public class autocode extends LinearOpMode {
          backR.setDirection(DcMotor.Direction.FORWARD);//+
          frontL.setDirection(DcMotor.Direction.REVERSE);//-
          frontR.setDirection(DcMotor.Direction.REVERSE);//+;
+//         straight();
+//
 
-             strafeRight(100,0.5,400);
-             straight(100,0.7,250);
-             strafeLeft(100,0.5,400);
      }
          void strafeRight(int distance,double power,long sleeptime){
               backL.setTargetPosition(-distance + backL.getCurrentPosition());
@@ -54,11 +53,32 @@ public class autocode extends LinearOpMode {
          }
 
 
-            void straight(int distance,double power,long sleeptime){
+         void straight(int distance,double power,long sleeptime){
 
+            backL.setTargetPosition(distance + backL.getCurrentPosition());
+            frontL.setTargetPosition(distance + frontL.getCurrentPosition());
+            backR.setTargetPosition(distance + backR.getCurrentPosition());
+            frontR.setTargetPosition(distance + frontR.getCurrentPosition());
+
+            backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            backL.setPower(power);
+            backR.setPower(power);
+            frontL.setPower(power);
+            frontR.setPower(power);
+
+            sleep(sleeptime);
+
+            }
+
+
+            void strafeLeft(int distance,double power,long sleeptime) {
                 backL.setTargetPosition(distance + backL.getCurrentPosition());
-                frontL.setTargetPosition(distance + frontL.getCurrentPosition());
-                backR.setTargetPosition(distance + backR.getCurrentPosition());
+                frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
+                backR.setTargetPosition(-distance + backR.getCurrentPosition());
                 frontR.setTargetPosition(distance + frontR.getCurrentPosition());
 
                 backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -67,37 +87,40 @@ public class autocode extends LinearOpMode {
                 frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
                 backL.setPower(power);
-                backR.setPower(power);
-                frontL.setPower(power);
+                backR.setPower(-power);
+                frontL.setPower(-power);
                 frontR.setPower(power);
 
                 sleep(sleeptime);
+
             }
+            void reverse (int distance,double power,long sleeptime){
 
+                backL.setTargetPosition(-distance + backL.getCurrentPosition());
+                frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
+                backR.setTargetPosition(-distance + backR.getCurrentPosition());
+                frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
 
-            void strafeLeft(int distance,double power,long sleeptime){
-                    backL.setTargetPosition(distance + backL.getCurrentPosition());
-                    frontL.setTargetPosition(-distance + frontL.getCurrentPosition() );
-                    backR.setTargetPosition(-distance + backR.getCurrentPosition());
-                    frontR.setTargetPosition(distance + frontR.getCurrentPosition());
+                backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-                    backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                backL.setPower(-power);
+                backR.setPower(-power);
+                frontL.setPower(-power);
+                frontR.setPower(-power);
 
-                    backL.setPower(power);
-                    backR.setPower(-power);
-                    frontL.setPower(-power);
-                    frontR.setPower(power);
+                sleep(sleeptime);
 
-                    sleep(sleeptime);
-
-
-
+                }
 
     }
 
-    
 
-}
+
+
+
+
+
+
