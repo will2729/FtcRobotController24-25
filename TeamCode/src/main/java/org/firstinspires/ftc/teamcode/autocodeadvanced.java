269d4cdc0299fbfcdd2ed82ffbc0149d3d2e368e
CyberEaglesAmethyst1 <147578354+CyberEaglesAmethyst1@users.noreply.github.com>
2024-12-09 19:05:11 -0700

    pushy mushy
    fixed sevos hopfuly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocodeadvanced.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocodeadvanced.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
 @Autonomous (name = "AutoMcTickler: Premium Edition", group = "Robot")
 public class autocodeadvanced extends LinearOpMode {
 
@@ -25,221 +24,193 @@ public class autocodeadvanced extends LinearOpMode {
 
     Servo rotator;
 
+    public void runOpMode() {
+        backL = hardwareMap.get(DcMotor.class, "bl");
+        frontL = hardwareMap.get(DcMotor.class, "fl");
+        backR = hardwareMap.get(DcMotor.class, "br");
+        frontR = hardwareMap.get(DcMotor.class, "fr");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        armextend = hardwareMap.get(DcMotor.class, "armextend");
 
+        backL.setDirection(DcMotor.Direction.REVERSE);//-
+        backR.setDirection(DcMotor.Direction.FORWARD);//+
+        frontL.setDirection(DcMotor.Direction.REVERSE);//-
+        frontR.setDirection(DcMotor.Direction.REVERSE);//+;
+        arm.setDirection(DcMotor.Direction.REVERSE);
+        armextend.setDirection(DcMotor.Direction.REVERSE);
 
-     public void runOpMode() {
-         backL = hardwareMap.get(DcMotor.class, "bl");
-         frontL = hardwareMap.get(DcMotor.class, "fl");
-         backR = hardwareMap.get(DcMotor.class, "br");
-         frontR = hardwareMap.get(DcMotor.class, "fr");
-         arm = hardwareMap.get(DcMotor.class, "arm");
-         armextend = hardwareMap.get(DcMotor.class, "armextend");
+        backL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-         backL.setDirection(DcMotor.Direction.REVERSE);//-
-         backR.setDirection(DcMotor.Direction.FORWARD);//+
-         frontL.setDirection(DcMotor.Direction.REVERSE);//-
-         frontR.setDirection(DcMotor.Direction.REVERSE);//+;
-         arm.setDirection(DcMotor.Direction.REVERSE);
-         armextend.setDirection(DcMotor.Direction.REVERSE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armextend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-         backL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         backR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         frontL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         frontR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         armextend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-         grabber  = hardwareMap.get(CRServo.class, "s_grab");
-         rotator = hardwareMap.get(Servo.class, "s_rot");
-
-
-
-
-
+        grabber = hardwareMap.get(CRServo.class, "s_grab");
+        rotator = hardwareMap.get(Servo.class, "s_rot");
 //
-         waitForStart();
+        waitForStart();
         // strafeLeft(200, 0.7);
         // sleepy(500);
-         armangle(2500, 0.3);
-         arm.setTargetPosition(arm.getCurrentPosition());
-         armlength(2750, 0.3);
-         armextend.setTargetPosition(armextend.getCurrentPosition());
-         servrot(0.7);
-         grabgo(-1);
-         sleep(500);
-         grabgo(0);
+        armangle(2500, 0.3);
+        arm.setTargetPosition(arm.getCurrentPosition());
+        armlength(2750, 0.3);
+        armextend.setTargetPosition(armextend.getCurrentPosition());
+        servrot(0.7);
+        grabgo(-1);
+        sleep(500);
+        grabgo(0);
+        sleep(1000);
+        armlength(100, 0.3);
+        armextend.setTargetPosition(armextend.getCurrentPosition());
+        servrot(0.5);
+        armangle(100, 0.25);
+        arm.setTargetPosition(arm.getCurrentPosition());
         // strafeRight(300, 0.7);
-         frontL.setPower(0.25);
-         backL.setPower(0.25);
-         frontR.setPower(-0.25);
-         backR.setPower(-0.25);
-         sleep(30000);
-
-
-
-
+        frontL.setPower(0.25);
+        backL.setPower(0.25);
+        frontR.setPower(-0.25);
+        backR.setPower(-0.25);
+        sleep(30000);
 //
 // every 7.1350849256900215 ticks = 1mm
-     }
-         void strafeRight(int distance,double power){
-              backL.setTargetPosition  (-distance + frontR.getCurrentPosition());
-             frontL.setTargetPosition(distance + frontL.getCurrentPosition());
-             backR.setTargetPosition(distance + backR.getCurrentPosition());
-             frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
+    }
 
-             backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-             backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-             frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-             frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    void strafeRight(int distance, double power) {
+        backL.setTargetPosition(-distance + frontR.getCurrentPosition());
+        frontL.setTargetPosition(distance + frontL.getCurrentPosition());
+        backR.setTargetPosition(distance + backR.getCurrentPosition());
+        frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
 
-             backL.setPower(power);
-             backR.setPower(power);
-             frontL.setPower(power);
-             frontR.setPower(power);
-         }
+        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-         void armlength(int distance, double power){
-         armextend.setTargetPosition(distance + armextend.getCurrentPosition());
+        backL.setPower(power);
+        backR.setPower(power);
+        frontL.setPower(power);
+        frontR.setPower(power);
+    }
 
-         armextend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    void armlength(int distance, double power) {
+        armextend.setTargetPosition(distance + armextend.getCurrentPosition());
 
-         armextend.setPower(power);
-         
-         while (armextend.isBusy()){
-                 idle();
-             }
-         }
-         void armangle(int distance, double power) {
-             arm.setTargetPosition(distance + arm.getCurrentPosition());
+        armextend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-             arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armextend.setPower(power);
 
-             arm.setPower(power);
+        while (armextend.isBusy()) {
+            idle();
+        }
+    }
+    void armangle(int distance, double power) {
+        arm.setTargetPosition(distance + arm.getCurrentPosition());
 
-             while (arm.isBusy()) {
-                 idle();
-             }
-         }
-         void grabgo(double power){
-         grabber.setPower(power);
-         }
-         void servrot(double distance){
-         rotator.setPosition(distance + rotator.getPosition());
-         }
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        arm.setPower(power);
 
+        while (arm.isBusy()) {
+            idle();
+        }
+    }
+    void grabgo(double power) {
+        grabber.setPower(power);
+    }
 
+    void servrot(double distance) {
+        rotator.setPosition(distance);
+    }
 
-         void straight(int distance,double power){
+    void straight(int distance, double power) {
 
-            backL.setTargetPosition(distance + backL.getCurrentPosition());
-            frontL.setTargetPosition(distance + frontL.getCurrentPosition());
-            backR.setTargetPosition(distance + backR.getCurrentPosition());
-            frontR.setTargetPosition(distance + frontR.getCurrentPosition());
+        backL.setTargetPosition(distance + backL.getCurrentPosition());
+        frontL.setTargetPosition(distance + frontL.getCurrentPosition());
+        backR.setTargetPosition(distance + backR.getCurrentPosition());
+        frontR.setTargetPosition(distance + frontR.getCurrentPosition());
 
-            backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            backL.setPower(power);
-            backR.setPower(power);
-            frontL.setPower(power);
-            frontR.setPower(power);
-
-
-
-            }
-
-
-            void strafeLeft(int distance,double power) {
-                backL.setTargetPosition(distance + backL.getCurrentPosition());
-                frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
-                backR.setTargetPosition(-distance + backR.getCurrentPosition());
-                frontR.setTargetPosition(distance + frontR.getCurrentPosition());
-
-                backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                backL.setPower(power);
-                backR.setPower(power);
-                frontL.setPower(power);
-                frontR.setPower(power);
-
-
-
-            }
-            void reverse (int distance,double power){
-
-                backL.setTargetPosition(-distance + backL.getCurrentPosition());
-                frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
-                backR.setTargetPosition(-distance + backR.getCurrentPosition());
-                frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
-
-                backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                backL.setPower(power);
-                backR.setPower(power);
-                frontL.setPower(power);
-                frontR.setPower(power);
-
-
-
-                }
-
-                void turn_left(int distance, double power){
-
-                    backL.setTargetPosition(-distance + backL.getCurrentPosition());
-                    frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
-                    backR.setTargetPosition(distance + backR.getCurrentPosition());
-                    frontR.setTargetPosition(distance + frontR.getCurrentPosition());
-
-                    backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                    backL.setPower(power);
-                    backR.setPower(power);
-                    frontL.setPower(power);
-                    frontR.setPower(power);
-
-
-
-
-                    }
-
-                    void turn_right(int distance, double power){
-
-                        backL.setTargetPosition(distance + backL.getCurrentPosition());
-                        frontL.setTargetPosition(distance + frontL.getCurrentPosition());
-                        backR.setTargetPosition(-distance + backR.getCurrentPosition());
-                        frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
-
-                        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                        backL.setPower(power);
-                        backR.setPower(power);
-                        frontL.setPower(power);
-                        frontR.setPower(power);
-
-
-                    }
+        backL.setPower(power);
+        backR.setPower(power);
+        frontL.setPower(power);
+        frontR.setPower(power);
     }
 
 
+    void strafeLeft(int distance, double power) {
+        backL.setTargetPosition(distance + backL.getCurrentPosition());
+        frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
+        backR.setTargetPosition(-distance + backR.getCurrentPosition());
+        frontR.setTargetPosition(distance + frontR.getCurrentPosition());
 
+        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        backL.setPower(power);
+        backR.setPower(power);
+        frontL.setPower(power);
+        frontR.setPower(power);
+    }
 
+    void reverse(int distance, double power) {
 
+        backL.setTargetPosition(-distance + backL.getCurrentPosition());
+        frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
+        backR.setTargetPosition(-distance + backR.getCurrentPosition());
+        frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
 
+        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        backL.setPower(power);
+        backR.setPower(power);
+        frontL.setPower(power);
+        frontR.setPower(power);
+    }
+
+    void turn_left(int distance, double power) {
+
+        backL.setTargetPosition(-distance + backL.getCurrentPosition());
+        frontL.setTargetPosition(-distance + frontL.getCurrentPosition());
+        backR.setTargetPosition(distance + backR.getCurrentPosition());
+        frontR.setTargetPosition(distance + frontR.getCurrentPosition());
+
+        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        backL.setPower(power);
+        backR.setPower(power);
+        frontL.setPower(power);
+        frontR.setPower(power);
+    }
+
+    void turn_right(int distance, double power) {
+
+        backL.setTargetPosition(distance + backL.getCurrentPosition());
+        frontL.setTargetPosition(distance + frontL.getCurrentPosition());
+        backR.setTargetPosition(-distance + backR.getCurrentPosition());
+        frontR.setTargetPosition(-distance + frontR.getCurrentPosition());
+
+        backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        backL.setPower(power);
+        backR.setPower(power);
+        frontL.setPower(power);
+        frontR.setPower(power);
+    }
+}
